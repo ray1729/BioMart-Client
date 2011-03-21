8fa2f044a0b419be7fc8ffa757353ed9f1f976db
Ray Miller <ray@1729.org.uk>
2011-03-21 22:13:27 +0000

    Add tests against the I-DCC biomart service

--- test/biomart_client/core_test.clj
+++ test/biomart_client/core_test.clj
@@ -13,3 +13,16 @@
                 result (query ds :filter {:chromosome_name "6" :hgnc_symbol "HLA-A"}
                                  :attrs ["ensembl_gene_id" "hgnc_symbol"])]
             (is (= '(("ENSG00000206503" "HLA-A")) result))))))))
+
+(deftest idcc-test
+  (let [idcc-url "http://www.i-dcc.org/biomart"]
+    (testing "Connecting to I-DCC and retrieving marts"
+      (let [ms (martservice idcc-url)]
+        (is (map? (:marts ms)))
+        (is (not (nil? ((:marts ms) "ikmc"))))
+        (is (not (nil? ((:datasets ms) "dcc"))))
+        (testing "Query dcc mart"
+          (let [ds (dataset ms "dcc")]
+            (is (not (nil? ds)))
+            (let [res (query ds :filter { :marker_symbol "Art4" } :attrs [ "marker_symbol" "mgi_accession_id" ])]
+              (is (= '(("Art4" "MGI:1202710")) res)))))))))
