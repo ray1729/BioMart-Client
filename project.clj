eca738ecd14a4968618f309bf24d937e70472b6b
chapmanb <chapmanb@50mail.com>
2011-03-22 09:12:08 -0400

    Move to clojure 1.2.1 and do not use snapshot for contrib

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject biomart-client "1.0.0-SNAPSHOT"
   :description "Clojure client interface to BioMart"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
