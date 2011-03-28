bf7c82210393cb7f606aa3e935ad71fc3edb24e0
Ray Miller <ray@1729.org.uk>
2011-03-28 08:23:17 +0100

    Another change to the query API. Datasets are now passed as hash-maps rather than using the 'dataset' function. This makes it easier to grab the attribute names, which can then be used to construct the map returned by the results parser. It also allows the opts argument to be omitted.
    Add a 'coerce-val' function to the results parser. At the moment, this
    just coerces strings that look like integers into integers, but could
    easily be expanded to deal with other data types.

--- src/biomart_client/query.clj
+++ src/biomart_client/query.clj
@@ -6,33 +6,34 @@
   (:require [clj-http.client :as http]))
 
 (def *default-query-opts* {:formatter            "TSV"
-                           :header               true
+                           :header               false
                            :uniqueRows           true
                            :count                false
                            :datasetConfigVersion "0.6"
                            :virtualSchemaName    "default"})
 
-(defn dataset
-  "Specify the dataset, filter and attributes for the query."
-  ([name filter attrs]
-     (dataset name filter attrs "default"))
-  ([name filter attrs interface]
-     (letfn [(coll-to-csv [s] (if (coll? s) (join "," (map as-str (seq s))) (as-str s)))
-             (attr-spec   [a] (vector :Attribute {:name (as-str a)}))
-             (filter-spec [f] (vector :Filter {:name (as-str (key f)) :value (coll-to-csv (val f))}))]
-       (vec (remove nil? (vector :Dataset {:name name :interface interface}
-                                 (when attrs (map attr-spec attrs))
-                                 (when filter (map filter-spec filter))))))))
+(defn- dataset
+  "Convert a dataset specification into a data structure that can be
+  passed to prxml"
+  [{:keys [dataset filter attrs interface] :or {:interface "default"}}]
+  (letfn [(coll-to-csv [s] (if (coll? s) (join "," (map as-str (seq s))) (as-str s)))
+          (attr-spec   [a] (vector :Attribute {:name (as-str a)}))
+          (filter-spec [f] (vector :Filter {:name (as-str (key f)) :value (coll-to-csv (val f))}))]
+    (vec (remove nil? (vector :Dataset {:name dataset :interface interface}
+                              (when attrs (map attr-spec attrs))
+                              (when filter (map filter-spec filter)))))))
 
 (defn- build-query-xml
   "Build XML required to perform martservice query."
   [opts datasets]
+  (when-not (or (= 1 (count datasets)) (= 2 (count datasets)))
+    (throwf "query requires 1 or 2 datasets"))
   (let [boolean->str (fn [b] (cond
                              (identical? b true) "1"
                              (identical? b false) "0"
                              :else b))
         opts (zipmap (keys opts) (map boolean->str (vals opts)))]
-    (with-out-str (prxml [:decl!] [:doctype! "Query"] [:Query opts datasets]))))
+    (with-out-str (prxml [:decl!] [:doctype! "Query"] [:Query opts (map dataset datasets)]))))
 
 (defn- wrap-biomart-errors
   "Examine the response body and throw an exception if any BioMart errors
@@ -54,32 +55,24 @@
   "Parse a string containing query results in TSV format. Returns a
   seq of maps keyed on column headers parsed from the first line of
   the response body."
-  [res-body]
-  (let [col-name->keyword (fn [c] (->> c trim (replace-re #"\s+" "_") lower-case keyword))
-        rows (parse-tsv res-body)
-        cols (map col-name->keyword (first rows))]
-    (map #(zipmap cols %) (rest rows))))
-
-(defn- parse-count
-  "Parse a string containing count result returned by
-  martservice. This should be one or more digits followed by a
-  newline. Throw an error if body is not in expected format"
-  [body]
-  (if-let [r (re-seq #"^\d+$" body)]
-    (Integer/parseInt (first r))
-    (throwf "Biomart error: cannot parse count '%s'" body)))
+  [res-body attrs]
+  (letfn [(coerce-val
+           [v]
+           (cond
+            (re-seq #"^\d+$" v) (Integer/parseInt v)
+            :else v))]
+    (let [rows (parse-tsv res-body)]
+      (map #(zipmap attrs (map coerce-val %)) rows))))
 
 (defn query
   "Query the specified datasets and return parsed results. For a
   normal query, a seq of maps keyed on column name is returned. When a
   count is requested (by passing opts {:count true}) the response will
-  simply be an integer."
-  [martservice-url opts & datasets]
-  (when-not (or (= 1 (count datasets)) (= 2 (count datasets)))
-    (throwf "query requires 1 or 2 datasets"))
-  (let [opts      (merge *default-query-opts* opts)
-        query-xml (build-query-xml opts datasets)
+  simply be a single row with key :count."
+  [martservice-url & args]
+  (let [datasets  (filter :dataset args)
+        opts      (or (first (remove :dataset args)) {})
+        attrs     (if (opts :count) [:count] (apply concat (map :attrs datasets)))
+        query-xml (build-query-xml (merge *default-query-opts* opts) datasets)
         res-body  (fetch-query-results martservice-url query-xml)]
-    (if (opts :count)
-      (parse-count res-body)
-      (parse-query-results res-body))))
+    (parse-query-results res-body attrs)))
--- test/biomart_client/query_test.clj
+++ test/biomart_client/query_test.clj
@@ -7,9 +7,9 @@
   (testing "Querying Ensembl martservice"
     (let [ms (martservice-url "http://www.ensembl.org/biomart")]
       (is ((comp not nil?) (re-find #"/martservice$" ms)))
-      (let [rs (query ms {} (dataset "hsapiens_gene_ensembl"
-                                     {:chromosome_name "6" :hgnc_symbol "HLA-A"}
-                                     [:ensembl_gene_id :hgnc_symbol]))
+      (let [rs (query ms {:dataset "hsapiens_gene_ensembl"
+                          :filter  {:chromosome_name "6" :hgnc_symbol "HLA-A"}
+                          :attrs   [:ensembl_gene_id :hgnc_symbol]})
             r (first rs)]
         (is (map? r))
         (is (:ensembl_gene_id r) "ENSG00000206503")
@@ -19,28 +19,29 @@
   (testing "Querying IDCC martservice"
     (let [ms (martservice-url  "http://www.i-dcc.org/biomart")]
       (is ((comp not nil?) (re-find #"/martservice$" ms)))
-      (let [rs (query ms {} (dataset "dcc"
-                                     {:marker_symbol "Art4"}
-                                     [:marker_symbol :mgi_accession_id]))
+      (let [rs (query ms {:dataset "dcc"
+                          :filter  {:marker_symbol "Art4"}
+                          :attrs   [:marker_symbol :mgi_accession_id]})
             r  (first rs)]
         (is (map? r))
         (is (:marker_symbol r) "Art4")
         (is (:mgi_accession_id r) "MGI:1202710"))
-      (let [rs (query ms {:count true} (dataset "dcc"
-                                                {:marker_symbol '(art4 abcd1 cbx1)}
-                                                [:mgi_accession_id]))]
-        (is (= rs 3))))))
+      (let [rs (query ms {:count true} {:dataset "dcc"
+                                        :filter  {:marker_symbol '(art4 abcd1 cbx1)}
+                                        :attrs   [:mgi_accession_id]})]
+        (is (= (:count (first rs)) 3))))))
 
 (deftest federated-query-test
   (testing "Federated query via IDCC martservice"
     (let [ms       "http://www.i-dcc.org/biomart/martservice"
-          targ_rep (dataset "idcc_targ_rep" {}
-                            ["ikmc_project_id" "mgi_accession_id" "targeting_vector" "escell_clone" "pipeline"])
-          kermits  (dataset "kermits" {:status [ "Genotype Confirmed" "Germline transmission achieved"]}
-                            ["status" "emma" "mi_centre" "mi_date"])
-          results  (query ms {} targ_rep kermits)]
+          targ_rep {:dataset "idcc_targ_rep"
+                    :attrs   [:ikmc_project_id :mgi_accession_id :targeting_vector :escell_clone :pipeline]}
+          kermits  {:dataset "kermits"
+                    :filter  {:status [ "Genotype Confirmed" "Germline transmission achieved"]}
+                    :attrs   [:status :emma :mi_centre :mi_date]}
+          results  (query ms targ_rep kermits)]
       (is (seq? results))
       (is (map? (first results)))
       (is (:pipeline (first results)))
       (is (:ikmc_project_id (first results)))
-      (is (:microinjection_centre (first results))))))
+      (is (:mi_centre (first results))))))
